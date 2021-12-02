6f53be7dfa1360e1621eaa715796be711d029fdb
Ankush Gupta <me@ankushg.com>
2021-12-01 21:13:04 -0800

    feat: create day00 package/template to copy/paste for future days

---
+++ src/main/kotlin/day00/Day00.kt
@@ -0,0 +1,21 @@
+package day00
+
+import AdventOfCodeDaySolution
+
+fun main() {
+    Day00.main()
+}
+
+object Day00 : AdventOfCodeDaySolution<Int>(
+    dayNumber = "00",
+    part1ExpectedAnswer = 0,
+    part2ExpectedAnswer = 0
+) {
+    override fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    override fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
